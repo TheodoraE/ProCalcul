0fc6bf7b60124763eb849f6441b5be75165f5677
TheodoraE <tidoraa@gmail.com>
2021-02-16 18:09:58 +0100

    save

--- app/Http/Controllers/CalculController.php
+++ app/Http/Controllers/CalculController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Aliment;
 use Illuminate\Http\Request;
 
 class CalculController extends Controller
 {
     public function index(){
-        
-        return view('welcome');
+        $repertories = Aliment::all();
+        return view('welcome', compact('repertories'));
     }
 
 
--- resources/views/partials/welcomeMid.blade.php
+++ resources/views/partials/welcomeMid.blade.php
@@ -1,25 +1,29 @@
-<div class="m-5 d-flex">
+<div class="m-5 d-flex align-items-center">
 
-    <div class="col-2">
+    <div class="col-2 my-5">
         <label for="">Insérez la quantité en grammes (g)</label>
         <input type="number" name="quantity">
     </div>
 
-    <div class="col-4">
-        <label for="">Sélectionnez l'aliment : </label>
-        <Select></Select>
-    </div>
+    <div class="form-group col-4 my-5">
+        <label for="exampleFormControlSelect1">Sélectionnez l'aliment : </label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            @foreach ($repertories as $repertory)
+                <option>{{$repertory->name}}</option>
+            @endforeach
+        </select>
+      </div>
 
-    <div class="col-2">
+    <div class="col-2 my-5">
         <button type="button" class="btn btn-primary">Calculer</button>
     </div>
 
-    <div class="col-2">
+    <div class="col-2 my-5">
         <Span>Résultat :</Span>
-        <span> g</span>
+        <span> <b class="text-danger">X</b> g</span>
     </div>
 
-    <div class="col-2">
+    <div class="col-2 my-5">
         <form action="">
             @csrf
             <button type="submit" class="btn btn-success">Ajouter à l'apport journalier</button>
