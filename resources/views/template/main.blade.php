4351c41b47bdaa19b40f79e1cab73ab31c74970a
TheodoraE <tidoraa@gmail.com>
2021-06-01 17:21:00 +0200

    test heroku

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ProCalcul</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
 </head>
 <body>
     {{-- Header --}}
@@ -20,6 +20,6 @@
 
     
     {{-- JavaScript --}}
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{secure_asset('js/app.js')}}"></script>
 </body>
 </html>
