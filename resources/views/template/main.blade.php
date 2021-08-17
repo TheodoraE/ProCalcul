aa818b746f7c74cc16e1cc3d0efa0d4e81f92751
TheodoraE <tidoraa@gmail.com>
2021-08-17 19:19:48 +0200

    view heroku DB1

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -10,8 +10,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        {{-- <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}"> --}}
+        {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     </head>
 
     <body>
@@ -29,8 +29,8 @@
         </div>
 
         {{-- JavaScript --}}
-        <script src="{{ asset('js/app.js') }}"></script>
-        {{-- <script src="{{ secure_asset('js/app.js') }}"></script> --}}
+        {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+        <script src="{{ secure_asset('js/app.js') }}"></script>
     </body>
 
 </html>
